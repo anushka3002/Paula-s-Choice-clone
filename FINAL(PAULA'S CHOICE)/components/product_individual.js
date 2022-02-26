3aec325d183bb287f65de1d3532bf5c522cbb7e8
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-02-26 19:33:37 +0530

    Update product_individual.js

--- FINAL(PAULA'S CHOICE)/components/product_individual.js
+++ FINAL(PAULA'S CHOICE)/components/product_individual.js
@@ -278,7 +278,7 @@ var Aindividual=JSON.parse(localStorage.getItem("paulasChoice"));
             div.append(div2,name,about,price,div3)
             div.setAttribute("class","givehoverstyle")
             div.onclick=function(){
-                window.location.href="product_individual.html"
+                // window.location.href="product_individual.html"
             }
             document.querySelector("#Are").append(div);
 
@@ -288,7 +288,7 @@ var Aindividual=JSON.parse(localStorage.getItem("paulasChoice"));
             arr.push(elem)
             div.addEventListener("click",senddata);
             function senddata(){
-                localStorage.setItem("paulasChoice",JSON.stringify(arr))
+                // localStorage.setItem("paulasChoice",JSON.stringify(arr))
             }
         })
     }
